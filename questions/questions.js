f341835692a1037628b5182cfe1dfc82ae518d6a
GitHub-Harrison <harrison-johnson@outlook.com>
2022-11-01 13:58:24 +0000

    Add functionality by using selectors inside the element

--- questions/questions.js
+++ questions/questions.js
@@ -1,5 +1,21 @@
 // using selectors inside the element
 
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function() {
+
+        // removes show-text from articles that do not match the one clicked
+        questions.forEach(function(item){
+            if (item !== question){
+                item.classList.remove('show-text');
+            }
+        });
+
+        question.classList.toggle('show-text');
+    });
+});
 
 
 // traversing the dom

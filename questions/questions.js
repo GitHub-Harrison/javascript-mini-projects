4e6e392d1a8dd27355c6cfc5aafec897144de5ca
GitHub-Harrison <harrison-johnson@outlook.com>
2022-11-01 13:48:24 +0000

    Add functionality via traversing the dom

--- questions/questions.js
+++ questions/questions.js
@@ -1,2 +1,14 @@
 // using selectors inside the element
+
+
+
 // traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
+});

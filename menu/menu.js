611782327f16948f64d1eeb3487f611f2e54dd26
GitHub-Harrison <harrison-johnson@outlook.com>
2022-11-09 15:13:52 +0000

    Add dynamic filter button creation + format code

--- menu/menu.js
+++ menu/menu.js
@@ -79,48 +79,75 @@ const menu = [{
         desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
 ];
-
-const sectionCenter = document.querySelector('.section-center');
-const filterBtns = document.querySelectorAll('.filter-btn');
-
-// load items
-window.addEventListener('DOMContentLoaded', function() {
-    displayMenuItems(menu)
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+    displayMenuButtons();
 });
 
-// for items
-filterBtns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        const category = e.currentTarget.dataset.id;
-        const menuCategory = menu.filter(function(menuItem) {
-            if (menuItem.category === category) {
-                return menuItem;
+function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+        // console.log(item);
+
+        return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+
+    sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        function (values, item) {
+            if (!values.includes(item.category)) {
+                values.push(item.category);
+            }
+            return values;
+        },
+        ["all"]
+    );
+    const categoryBtns = categories
+        .map(function (category) {
+            return `<button type="button" class="filter-btn" data-id=${category}>
+          ${category}
+        </button>`;
+        })
+        .join("");
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+    console.log(filterBtns);
+
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (e) {
+            // console.log(e.currentTarget.dataset);
+            const category = e.currentTarget.dataset.id;
+            const menuCategory = menu.filter(function (menuItem) {
+                // console.log(menuItem.category);
+                if (menuItem.category === category) {
+                    return menuItem;
+                }
+            });
+            if (category === "all") {
+                diplayMenuItems(menu);
+            } else {
+                diplayMenuItems(menuCategory);
             }
         });
-        if (category === 'all'){
-            displayMenuItems(menu)
-        } else {
-            displayMenuItems(menuCategory)
-        }
     });
-});
-
-function displayMenuItems(menuItems) {
-    let displayMenu = menuItems.map(function(item) {
-        return `
-            <article class="menu-item">
-                <img src=${item.img} alt=${item.title} class="photo" />
-                <div class="item-info">
-                    <header>
-                        <h4>${item.title}</h4>
-                        <h4 class="price">${item.price}</h4>
-                    </header>
-                    <p class="item-text">
-                        ${item.desc}
-                    </p>
-                </div>
-            </article>`;
-    });
-    displayMenu = displayMenu.join('');
-    sectionCenter.innerHTML = displayMenu;
 }
